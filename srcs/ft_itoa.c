79e31d6a9c51230bb347c606b8ce7f8d7b053b39
Lisa TRAN <ltran@e1r8p3.42.fr>
2017-06-30 15:39:27 +0200

    update de itoa

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -6,63 +6,30 @@
 /*   By: ltran <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/27 14:23:59 by ltran             #+#    #+#             */
-/*   Updated: 2017/06/23 15:31:39 by ltran            ###   ########.fr       */
+/*   Updated: 2017/06/30 15:35:37 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-static int		ft_i(int n)
-{
-	unsigned int	mod;
-	unsigned int	i;
-	unsigned int	div;
-
-	mod = 0;
-	i = 0;
-	if (n <= 0)
-	{
-		i++;
-		div = n * -1;
-	}
-	else
-		div = n;
-	while (div != 0 || div != mod)
-	{
-		mod = div % 10;
-		div = div / 10;
-		i++;
-		if (div == 0 && div == mod)
-			i--;
-	}
-	return (i);
-}
-
 char			*ft_itoa(int n)
 {
-	unsigned int	mod;
-	unsigned int	div;
-	int				i;
-	char			*tab;
+	int		i;
+	int		tmp;
+	char	*tab;
 
-	i = ft_i(n);
-	mod = 0;
-	tab = (char*)malloc((sizeof(char)) * (i + 1));
-	if (!(tab))
+	tmp = n;
+	i = tmp < 0 ? 2 : 1;
+	tmp = (tmp < 0) ? tmp * (-1) : tmp;
+	while ((tmp = tmp / 10) > 0)
+		++i;
+	if (!(tab = (char*)malloc((sizeof(char)) * (i + 1))))
 		return (NULL);
 	tab[i] = '\0';
-	i--;
-	div = n;
-	if (n < 0)
-		div = div * -1;
-	while (i >= 0)
-	{
-		mod = div % 10;
-		div = div / 10;
-		tab[i] = mod + '0';
-		i--;
-	}
-	if (n < 0)
-		tab[0] = '-';
+	tab[0] = '-';
+	tmp = n < 0 ? 0 : -1;
+	n = n < 0 ? n * -1 : n;
+	while (--i > tmp && (tab[i] = (n % 10) + '0'))
+		n = n / 10;
 	return (tab);
 }
