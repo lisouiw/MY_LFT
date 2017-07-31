0142713389bd5920fd2e26c25196feafd57cef85
Lisa TRAN <ltran@e2r13p1.42.fr>
2017-07-31 11:54:05 +0200

    new atoi protege

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,48 +6,37 @@
 /*   By: ltran <ltran@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 17:34:43 by ltran             #+#    #+#             */
-/*   Updated: 2017/06/23 15:30:44 by ltran            ###   ########.fr       */
+/*   Updated: 2017/07/31 11:52:08 by ltran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-static int		ft_vide(const char *str)
-{
-	int		i;
-
-	i = 0;
-	while (str[i] == '\f' || str[i] == '\v' || str[i] == '\r' ||
-	str[i] == '\n' || str[i] == ' ' || str[i] == '\t')
-		i++;
-	return (i);
-}
-
 int				ft_atoi(const char *str)
 {
-	int					i;
 	unsigned int		nb;
 	int					ent;
 	int					sgn;
 
-	i = ft_vide(str);
 	nb = 0;
 	ent = 0;
 	sgn = 0;
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			sgn = 1;
-		i++;
-	}
-	else if (!ft_isdigit(str[i]))
+	while (*str == '\f' || *str == '\v' || *str == '\r' ||
+		*str == '\n' || *str == ' ' || *str == '\t')
+		++str;
+	if (*str == '-' && ft_strlen(str) >= 10 &&
+			ft_strcmp("-2147483648", str) <= 0)
+		return (-2147483648);
+	if (*str == '+' || ((*str == '-') && (sgn = 1)))
+		++str;
+	if (ft_strlen(str) >= 10 && ft_strcmp("2147483647", str) <= 0)
+		return (2147483647);
+	else if (!ft_isdigit(*str))
 		return (0);
-	while (str[i] >= '0' && str[i] <= '9')
+	while (*str >= '0' && *str <= '9')
 	{
-		ent = str[i] - 48;
-		nb = nb * 10 + ent;
-		i++;
+		nb = nb * 10 + (*str - 48);
+		++str;
 	}
-	sgn == 1 ? nb = -(nb) : nb;
-	return (nb);
+	return (nb = (sgn == 1) ? -(nb) : nb);
 }
